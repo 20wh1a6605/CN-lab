3c28e0f9fed2e003cc1088f4ce9302e90f301d3f
20wh1a6605 <93636597+20wh1a6605@users.noreply.github.com>
2022-12-28 14:02:40 +0530

    Add files via upload

---
+++ encrypt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int d;
+    char str[100];
+    printf("Give the data to be encrypted\n");
+    scanf("%[^\n]%*c",str);
+    printf("Give the key value\n");
+    scanf("%d",&d);
+    for(int i = 0;i<strlen(str);i++){
+        if(str[i] != ' '){
+            str[i] = str[i] + d;
+            }
+        }
+    printf("%s",str);
+    }
